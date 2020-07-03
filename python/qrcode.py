c7bc43d7ee68d96015617e9666ce5abd13b6bd30
rafmkyrie <rafainiestaretro@gmail.com>
2020-07-03 22:15:08 +0100

    update Rafa quasi-final

--- python/qrcode.py
+++ python/qrcode.py
@@ -7,15 +7,15 @@ import pyqrcode
 from PIL import Image
 
 class qrcode:
-	keys = ["Rafa", "Ghiles", "Raouf", "Chakib"]
-	
 	def __init__(self, id):
 			self.id = id
 			a = "https://rocky-harbor-71969.herokuapp.com/dossier?id="+self.id
-			url=pyqrcode.create(a)
+			url=pyqrcode.create("https://rocky-harbor-71969.herokuapp.com/dossier/")
 			
 			url.png(''+id+'.png', scale = 10)
 			
 			image1 = Image.open(r''+id+'.png')
 			im1 = image1.convert('RGB')
 			im1.save(r''+id+'myqr.pdf')
+
+a = qrcode('raouf')
